c7328e0f8d1fd0bcceea0ed16c2d0cbf2a819294
XcenaX <vlad-057@mail.ru>
2026-04-28 15:43:25 +0500

    Release desktop 3.0.2

--- desktop_app/config/app_info.py
+++ desktop_app/config/app_info.py
@@ -2,8 +2,9 @@
 
 APP_NAME = "Irios Инструменты"
 APP_ORGANIZATION = "Irios"
-APP_VERSION = "3.0.1"
+APP_VERSION = "3.0.2"
 UPDATE_MANIFEST_URL = "https://raw.githubusercontent.com/XcenaX/Irios-Tools/main/releases/latest.json"
+
 
 
 
